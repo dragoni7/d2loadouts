0af59708b7554edf5bc781c72a03b6a8665bb9eb
dragoni7 <sdgibson18@outlook.com>
2024-06-24 12:23:48 -0700

    Enabled localhost https. Fixed authenticated redirect

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import fs from 'fs'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ command, mode, isSsrBuild, isPreview }) => {
+  if (command === 'serve') {
+    return {
+      plugins: [react()],
+      server: {
+        https: {
+          key: fs.readFileSync('./certs/localhost.key'),
+          cert: fs.readFileSync('./certs/localhost.crt')
+        }
+      }
+    }
+  }
+  else {
+    return {
+      plugins: [react()],
+    }
+  }
 })
