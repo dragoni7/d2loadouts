4bd6304dc75ae5553361b6997c7cf87f8959f326
ahmedmgamal94 <98055904+ahmedmgamal94@users.noreply.github.com>
2024-07-12 13:30:33 -0700

    mods-subtraction/state-calculation

--- src/features/profile/DestinyProfile.ts
+++ src/features/profile/DestinyProfile.ts
@@ -8,17 +8,52 @@ export interface DestinyArmor {
   mobility: number;
   strength: number;
   recovery: number;
-  instanceHash: string;
-  itemHash: string;
-  artifice: boolean;
-  masterwork: boolean;
-  exotic: boolean;
-  class: string;
-  type: string;
-  socket: string;
+  instanceHash?: string;
+  itemHash?: string;
+  artifice?: boolean;
+  masterwork?: boolean;
+  exotic?: boolean;
+  class?: string;
+  type?: string;
+  socket?: string;
 }
-
+const modRemoveValue:{ [key: string]: (armor: DestinyArmor) => void }= {
+  '2724608735': (armor:DestinyArmor) => armor.intellect = armor.intellect - 10,
+  '3897511453': (armor:DestinyArmor) => armor.intellect = armor.intellect - 5,
+  '3160845295': (armor:DestinyArmor) => armor.intellect = armor.intellect - 3,
+  '1180408010': (armor:DestinyArmor) => armor.resilience = armor.resilience - 10,
+  '2532323436': (armor:DestinyArmor) => armor.resilience = armor.resilience - 5,
+  '199176566': (armor:DestinyArmor) => armor.resilience = armor.resilience - 3,
+  '1435557120': (armor:DestinyArmor) => armor.discipline = armor.discipline - 10,
+  '4021790309': (armor:DestinyArmor) => armor.discipline = armor.discipline - 5,
+  '617569843': (armor:DestinyArmor) => armor.discipline = armor.discipline - 3,
+  '204488676': (armor:DestinyArmor) => armor.recovery = armor.recovery - 10,
+  '1237786518': (armor:DestinyArmor) => armor.recovery = armor.recovery - 5,
+  '539459624': (armor:DestinyArmor) => armor.recovery = armor.recovery - 3,
+  '183296050': (armor:DestinyArmor) => armor.mobility = armor.mobility - 10,
+  '1703647492': (armor:DestinyArmor) => armor.mobility = armor.mobility - 5,
+  '2322202118': (armor:DestinyArmor) => armor.mobility = armor.mobility - 3,
+  '287799666': (armor:DestinyArmor) => armor.strength = armor.strength - 10,
+  '2639422088': (armor:DestinyArmor) => armor.strength = armor.strength - 5,
+  '2507624050': (armor:DestinyArmor) => armor.strength = armor.strength - 3,
+}
 const IS_ARMOR = 3897883278;
+const INTELLECT = 144602215;
+const RESILIENCE = 392767087;
+const DISCIPLINE = 1735777505;
+const RECOVERY = 1943323491;
+const MOBILITY = 2996146975;
+const STRENGTH = 4244567218;
+
+const HELMET = 3448274439;
+const ARMS = 3551918588;
+const CHEST = 14239492;
+const LEG = 20886954;
+const CLASS = 1585787867;
+
+const ARTIFICE_ARMOR = 3727270518;
+
+
 
 export async function getProfile(destinyMembershipId: string) {
   const accessToken = getTokens()?.accessToken.value;
@@ -48,12 +83,12 @@ export async function getProfile(destinyMembershipId: string) {
 
     console.log(armorHashes);
 
-    const destinyArmors= [];
+    const destinyArmors:DestinyArmor[]= [];
     for (const hash of armorHashes) {
       if (responseData.stats.data.hasOwnProperty(hash)) {
         const stats = responseData.stats.data[hash].stats;
         const statKeys = Object.keys(stats);
-        const destinyArmor= {
+        const destinyArmor:DestinyArmor= {
             intellect: stats[144602215]?.value || 0,
             discipline: stats[1735777505]?.value || 0,
             resilience: stats[392767087]?.value || 0,
@@ -62,17 +97,24 @@ export async function getProfile(destinyMembershipId: string) {
             recovery: stats[1943323491]?.value || 0,
             instanceHash: hash,
           };
-          
-          destinyArmors.push(destinyArmor);
+        if(responseData.sockets.data[hash].sockets){
+          for(const key in modRemoveValue){
+
+            if(responseData.sockets.data[hash].sockets.some((mod:any) => mod.plugHash === parseInt(key))){
+              modRemoveValue[key](destinyArmor)
+            }
+        }  
+        if(responseData.sockets.data[hash].sockets.some((mod:any) => mod.plugHash === ARTIFICE_ARMOR)){
+          destinyArmor.artifice = true;
+        }
+        else{
+          destinyArmor.artifice = false;
+        }
+        destinyArmors.push(destinyArmor);
       }
+      }  
     }
     console.log(destinyArmors)
-
-    for (const key in responseData.sockets.data) {
-    }
-
-    for (const key in responseData.perks.data) {
-    }
   } else {
     console.log("Could not get response");
   }
