7d67f0129dd548c2b43506a8487608a9637a14e1
dragoni7 <sdgibson18@outlook.com>
2024-07-11 17:48:39 -0700

    Began pulling profile data from api

--- src/features/profile/DestinyProfile.ts
+++ src/features/profile/DestinyProfile.ts
@@ -1,49 +1,62 @@
 import { _get } from "../../lib/bungie_api/BungieApiClient";
 import { getTokens } from "../../lib/bungie_api/TokensStore";
 
-export interface DestinyCharacter {
-  membershipId: string;
-  membershipType: number;
-  characterId: string;
-  dateLastPlayed: string;
-  classType: number;
-  classHash: number;
-  emblemPath: string;
-  emblemHash: number;
-  emblemBackgroundPath: string;
+export interface DestinyArmor {
+  intellect: number;
+  discipline: number;
+  resilience: number;
+  mobility: number;
+  strength: number;
+  recovery: number;
+  instanceHash: string;
+  itemHash: string;
+  artifice: boolean;
+  masterwork: boolean;
+  exotic: boolean;
+  class: string;
+  type: string;
+  socket: string;
 }
 
-export function getProfile(destinyMembershipId: string) {
-  const accessToken = getTokens()?.accessToken;
+const IS_ARMOR = 3897883278;
 
-  _get(
-    `/Platform/Destiny2/1/Profile/${destinyMembershipId}/?components=102,200`,
+export async function getProfile(destinyMembershipId: string) {
+  const accessToken = getTokens()?.accessToken.value;
+
+  const response = await _get(
+    `/Platform/Destiny2/1/Profile/${destinyMembershipId}/?components=102,201,300,205,302,304,305`,
     {
       headers: {
-        Authorization: `Bearer ${accessToken}}`,
+        Authorization: `Bearer ${accessToken}`,
       },
     }
-  ).then((response) => {
-    if (response.data.Response) {
-      var characters = response.data.Response.characters.data;
-      for (const key in characters) {
-        const character: DestinyCharacter = {
-          membershipId: characters[key].membershipId,
-          membershipType: characters[key].membershipType,
-          characterId: characters[key].characterId,
-          dateLastPlayed: characters[key].dateLastPlayed,
-          classType: characters[key].classType,
-          classHash: characters[key].classHash,
-          emblemPath: characters[key].emblemPath,
-          emblemHash: characters[key].emblemHash,
-          emblemBackgroundPath: characters[key].emblemBackgroundPath,
-        };
-      }
+  );
 
-      return true;
-    } else {
-      console.log("Could not get response");
-      return false;
+  if (response.data.Response) {
+    var responseData = response.data.Response.itemComponents;
+
+    var armorHashes = [];
+
+    for (const key in responseData.instances.data) {
+      if (
+        responseData.instances.data[key].primaryStat !== undefined &&
+        responseData.instances.data[key].primaryStat.statHash === IS_ARMOR
+      ) {
+        armorHashes.push(key);
+      }
     }
-  });
+
+    console.log(armorHashes);
+
+    for (const key in responseData.stats.data) {
+    }
+
+    for (const key in responseData.sockets.data) {
+    }
+
+    for (const key in responseData.perks.data) {
+    }
+  } else {
+    console.log("Could not get response");
+  }
 }
