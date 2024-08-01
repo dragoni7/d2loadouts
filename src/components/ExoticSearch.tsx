ca078d35d55c4672cbbbce6fc762dc7ef4698783
ahmedmgamal94 <98055904+ahmedmgamal94@users.noreply.github.com>
2024-08-01 14:48:36 -0700

    added on select for the exotics and cleaned ui

--- src/components/ExoticSearch.tsx
+++ src/components/ExoticSearch.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { styled } from '@mui/system';
-import { Autocomplete, TextField } from '@mui/material';
+import { Autocomplete, TextField, Popper } from '@mui/material';
 import { db } from '../store/db';
 import { Character } from '../types';
 
@@ -15,35 +15,64 @@ const NewComponentContainer = styled('div')({
   justifyContent: 'center',
 });
 
-const ExoticIcon = styled('img')({
-  width: '50px',
-  height: '50px',
-  marginRight: '10px',
-});
+const ExoticIcon = styled('img')<{ isOwned: boolean; isSelected: boolean }>(
+  ({ isOwned, isSelected }) => ({
+    width: isSelected ? '100px' : '50px',
+    height: isSelected ? '100px' : '50px',
+    marginRight: '10px',
+    filter: isOwned ? 'none' : 'grayscale(100%)',
+    border: isSelected ? '5px solid gold' : 'none',
+    boxShadow: isSelected ? '0 0 10px 2px gold' : 'none',
+  })
+);
 
-const ArrowButton = styled('div')({
-  width: '30px',
-  height: '30px',
+const ArrowButton = styled('div')<{ isSelected: boolean }>(({ isSelected }) => ({
+  width: isSelected ? '20px' : '30px',
+  height: isSelected ? '100px' : '30px',
   backgroundColor: 'rgba(255, 255, 255, 0.2)',
   display: 'flex',
   alignItems: 'center',
   justifyContent: 'center',
   cursor: 'pointer',
-  borderRadius: '50%',
+  borderRadius: '0',
   marginLeft: '10px',
-});
+  '&:hover': {
+    border: '1px solid white',
+  },
+}));
 
-const ArrowIcon = styled('div')({
+const ArrowIcon = styled('div')<{ isSelected: boolean }>(({ isSelected }) => ({
   width: 0,
   height: 0,
   borderLeft: '10px solid white',
   borderTop: '5px solid transparent',
   borderBottom: '5px solid transparent',
-});
+}));
 
 const SelectExotic = styled('span')({
   fontSize: '16px',
   fontWeight: 'bold',
+  color: 'white',
+  marginRight: '10px',
+});
+
+const StyledPopper = styled(Popper)({
+  '& .MuiAutocomplete-paper': {
+    backgroundColor: 'rgba(128, 128, 128, 0.9)',
+    color: 'white',
+    '&::-webkit-scrollbar': {
+      display: 'none',
+    },
+    '&': {
+      scrollbarWidth: 'none',
+    },
+  },
+  '& .MuiAutocomplete-option': {
+    '&:hover': {
+      backgroundColor: 'rgba(128, 128, 128, 0.7)',
+      border: '1px solid white',
+    },
+  },
 });
 
 type ExoticViewModel = {
@@ -83,6 +112,10 @@ const ExoticSearch: React.FC<ExoticSearchProps> = ({ selectedCharacter }) => {
     fetchExoticData();
   }, [selectedCharacter]);
 
+  useEffect(() => {
+    setSelectedExotic(null);
+  }, [selectedCharacter]);
+
   const handleClearSelection = () => {
     setSelectedExotic(null);
   };
@@ -95,8 +128,8 @@ const ExoticSearch: React.FC<ExoticSearchProps> = ({ selectedCharacter }) => {
           <Autocomplete
             disablePortal
             value={selectedExotic}
-            onChange={(event: any, newValue: ExoticViewModel | null) => {
-              setSelectedExotic(newValue);
+            onChange={(event, newValue) => {
+              setSelectedExotic(newValue as ExoticViewModel | null);
             }}
             inputValue={inputValue}
             onInputChange={(event, newInputValue) => {
@@ -106,21 +139,66 @@ const ExoticSearch: React.FC<ExoticSearchProps> = ({ selectedCharacter }) => {
             options={exotics}
             getOptionLabel={(option: ExoticViewModel) => option.name}
             sx={{ width: 300 }}
+            PopperComponent={StyledPopper}
             renderOption={(props, option) => (
-              <li {...props} style={{ color: option.isOwned ? 'inherit' : 'grey' }}>
-                <ExoticIcon src={option.icon} alt="Exotic Icon" />
+              <li
+                {...props}
+                style={{ color: option.isOwned ? 'inherit' : 'rgba(211, 211, 211, 0.7)' }}
+              >
+                <ExoticIcon
+                  src={option.icon}
+                  alt="Exotic Icon"
+                  isOwned={option.isOwned}
+                  isSelected={false}
+                />
                 {option.name}
               </li>
             )}
-            renderInput={(params) => <TextField {...params} label="Exotics" />}
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                label="Exotics"
+                variant="outlined"
+                sx={{
+                  '& .MuiOutlinedInput-root': {
+                    color: 'white',
+                    borderRadius: '0',
+                    '& fieldset': {
+                      borderColor: 'white',
+                    },
+                    '&:hover fieldset': {
+                      borderColor: 'white',
+                    },
+                    '&.Mui-focused fieldset': {
+                      borderColor: 'white',
+                    },
+                  },
+                  '& .MuiInputLabel-root': {
+                    color: 'white',
+                  },
+                  '& .MuiInputLabel-root.Mui-focused': {
+                    color: 'white',
+                  },
+                }}
+              />
+            )}
           />
         </div>
       ) : (
         <div style={{ display: 'flex', alignItems: 'center' }}>
-          {selectedExotic && <ExoticIcon src={selectedExotic.icon} alt="Exotic Icon" />}
-          <ArrowButton onClick={handleClearSelection}>
-            <ArrowIcon />
-          </ArrowButton>
+          {selectedExotic && (
+            <>
+              <ExoticIcon
+                src={selectedExotic.icon}
+                alt="Exotic Icon"
+                isOwned={selectedExotic.isOwned}
+                isSelected={true}
+              />
+              <ArrowButton isSelected={true} onClick={handleClearSelection}>
+                <ArrowIcon isSelected={true} />
+              </ArrowButton>
+            </>
+          )}
         </div>
       )}
     </NewComponentContainer>
