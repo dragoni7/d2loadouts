2a28cb3440945b6d2e53784f5fa38fdbbfc9ff06
dragoni7 <sdgibson18@outlook.com>
2024-08-08 21:22:00 -0700

    Added loadout config to redux

--- src/store/index.ts
+++ src/store/index.ts
@@ -3,12 +3,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import MembershipReducer from './MembershipReducer';
 import ProfileReducer from './ProfileReducer';
 import ModReducer from './ModSlice';
+import LoadoutReducer from './LoadoutReducer';
 
 export const store = configureStore({
   reducer: {
     destinyMembership: MembershipReducer,
     profile: ProfileReducer,
     mods: ModReducer,
+    loadoutConfig: LoadoutReducer,
   },
 });
 
