43cdcc7691d8608ad3a0d166c459877f5213fe3e
ahmedmgamal94 <98055904+ahmedmgamal94@users.noreply.github.com>
2024-08-11 13:29:52 -0700

    fixed single diamond prismatic duplicates

--- src/components/SingleDiamondButton.tsx
+++ src/components/SingleDiamondButton.tsx
@@ -19,36 +19,43 @@ const SingleDiamondButton: React.FC<SingleDiamondButtonProps> = ({
   const [lastNonPrismaticSubclass, setLastNonPrismaticSubclass] = useState<ManifestSubclass | null>(
     null
   );
-  const [showGrid, setShowGrid] = useState(true);
+  const [isPrismaticActive, setIsPrismaticActive] = useState(false);
+
+  const getDefaultSubclass = () => {
+    return subclasses.find((subclass) => !subclass.name.includes('Prismatic')) || subclasses[0];
+  };
 
   useEffect(() => {
-    if (!selectedSubclass && subclasses.length > 0) {
-      const defaultSubclass =
-        subclasses.find((subclass) => !subclass.name.includes('Prismatic')) || subclasses[0];
+    if (selectedSubclass) {
+      setCurrentSubclass(selectedSubclass);
+      setIsPrismaticActive(selectedSubclass.name.includes('Prismatic'));
+      if (!selectedSubclass.name.includes('Prismatic')) {
+        setLastNonPrismaticSubclass(selectedSubclass);
+      } else if (!lastNonPrismaticSubclass) {
+        setLastNonPrismaticSubclass(getDefaultSubclass());
+      }
+    } else if (subclasses.length > 0) {
+      const defaultSubclass = getDefaultSubclass();
       setCurrentSubclass(defaultSubclass);
       setLastNonPrismaticSubclass(defaultSubclass);
       onSubclassSelect(defaultSubclass);
-    } else if (selectedSubclass) {
-      setCurrentSubclass(selectedSubclass);
     }
   }, [selectedSubclass, subclasses, onSubclassSelect]);
 
   const handleSelect = (subclass: ManifestSubclass) => {
     if (subclass.name.includes('Prismatic')) {
-      setShowGrid(false);
-      setLastNonPrismaticSubclass(currentSubclass);
-      setCurrentSubclass(subclass);
+      setIsPrismaticActive(true);
     } else {
-      setShowGrid(true);
-      setLastNonPrismaticSubclass(currentSubclass);
-      setCurrentSubclass(subclass);
+      setIsPrismaticActive(false);
+      setLastNonPrismaticSubclass(subclass);
     }
+    setCurrentSubclass(subclass);
     onSubclassSelect(subclass);
   };
 
   const handleReset = () => {
-    setShowGrid(true);
     if (lastNonPrismaticSubclass) {
+      setIsPrismaticActive(false);
       setCurrentSubclass(lastNonPrismaticSubclass);
       onSubclassSelect(lastNonPrismaticSubclass);
     }
@@ -66,11 +73,11 @@ const SingleDiamondButton: React.FC<SingleDiamondButtonProps> = ({
 
   return (
     <div className="single-diamond-wrapper">
-      {showGrid && (
+      {!isPrismaticActive && (
         <div className="diamond-grid">
           {nonPrismaticSubclasses.map((subclass, index) => (
             <div
-              key={index}
+              key={subclass.itemHash}
               className={`diamond-button button-${index + 1}`}
               onClick={() => handleSelect(subclass)}
               onContextMenu={(event) => handleRightClick(event, subclass)}
@@ -88,31 +95,21 @@ const SingleDiamondButton: React.FC<SingleDiamondButtonProps> = ({
           <img src={currentSubclass.icon} alt={currentSubclass.name} className="diamond-icon" />
         )}
       </div>
-      {prismaticSubclass && !showGrid && (
+      {prismaticSubclass && (
         <div
           className="prismatic-button"
-          onClick={handleReset}
-          onContextMenu={(event) => handleRightClick(event, lastNonPrismaticSubclass!)}
-        >
-          {lastNonPrismaticSubclass && (
-            <img
-              src={lastNonPrismaticSubclass.icon}
-              alt={lastNonPrismaticSubclass.name}
-              className="diamond-icon"
-            />
-          )}
-        </div>
-      )}
-      {prismaticSubclass && showGrid && (
-        <div
-          className="prismatic-button"
-          onClick={() => handleSelect(prismaticSubclass)}
-          onContextMenu={(event) => handleRightClick(event, prismaticSubclass)}
+          onClick={isPrismaticActive ? handleReset : () => handleSelect(prismaticSubclass)}
+          onContextMenu={(event) =>
+            handleRightClick(
+              event,
+              isPrismaticActive ? lastNonPrismaticSubclass! : prismaticSubclass
+            )
+          }
         >
           <img
-            src={prismaticSubclass.icon}
-            alt={prismaticSubclass.name}
-            className="circular-icon"
+            src={isPrismaticActive ? lastNonPrismaticSubclass!.icon : prismaticSubclass.icon}
+            alt={isPrismaticActive ? lastNonPrismaticSubclass!.name : prismaticSubclass.name}
+            className={isPrismaticActive ? 'diamond-icon' : 'circular-icon'}
           />
         </div>
       )}
