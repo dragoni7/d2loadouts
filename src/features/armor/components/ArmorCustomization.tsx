9f4448ac32dd3f6052813c2df98b9cd9b9e3bc5f
ahmedmgamal94 <98055904+ahmedmgamal94@users.noreply.github.com>
2024-08-26 12:04:32 -0700

    added back button

--- src/features/armor/components/ArmorCustomization.tsx
+++ src/features/armor/components/ArmorCustomization.tsx
@@ -4,13 +4,26 @@ import LoadoutArmor from './LoadoutArmor';
 import EquipLoadout from '../../loadouts/components/EquipLoadout';
 import { ManifestSubclass } from '../../../types/manifest-types';
 import AbilitiesModification from '../../subclass/AbilitiesModification';
+import { Button, Box } from '@mui/material';
+import { styled } from '@mui/material/styles';
 
 interface ArmorCustomizationProps {
   onBackClick: () => void;
   screenshot: string;
   subclass: ManifestSubclass;
-  setSelectedSubclass: (subclass: ManifestSubclass) => void;
 }
+
+const TransparentButton = styled(Button)(({ theme }) => ({
+  background: 'transparent',
+  color: 'white',
+  padding: theme.spacing(1, 2),
+  '&:hover': {
+    background: 'rgba(255, 255, 255, 0.1)',
+  },
+  '& .MuiButton-startIcon': {
+    marginRight: theme.spacing(1),
+  },
+}));
 
 const ArmorCustomization: React.FC<ArmorCustomizationProps> = ({
   onBackClick,
@@ -19,6 +32,14 @@ const ArmorCustomization: React.FC<ArmorCustomizationProps> = ({
 }) => {
   return (
     <div className="armor-customization-wrapper" style={{ backgroundImage: `url(${screenshot})` }}>
+      <Box position="absolute" top={16} left={16} zIndex={1000}>
+        <TransparentButton
+          onClick={onBackClick}
+          startIcon={<span style={{ fontSize: '1.2em' }}>←</span>}
+        >
+          Back
+        </TransparentButton>
+      </Box>
       <div className="left-panel">
         <LoadoutArmor />
         <EquipLoadout />
