f6ed125cbd9d2abcb0c55ec929e86317a6dfcc0c
dragoni7 <sdgibson18@outlook.com>
2024-09-21 15:37:06 -0700

    Prevent selecting same stat

--- src/store/DashboardReducer.tsx
+++ src/store/DashboardReducer.tsx
@@ -27,7 +27,14 @@ const dashboardSlice = createSlice({
   initialState,
   reducers: {
     updateSelectedValues: (state, action: PayloadAction<{ [key: string]: number }>) => {
-      state.selectedValues = action.payload;
+      const keys = Object.keys(action.payload);
+
+      for (let i = 0; i < keys.length; i++) {
+        if (state.selectedValues[keys[i]] !== action.payload[keys[i]]) {
+          state.selectedValues = action.payload;
+          break;
+        }
+      }
     },
     updateSelectedExoticItemHash: (
       state,
