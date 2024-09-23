c3e0d7bcb67f25d7e54bb5f571c33189e6ee7447
dragoni7 <sdgibson18@outlook.com>
2024-09-23 15:19:58 -0700

    Fixed useSelectedCharacter default state

--- src/hooks/use-selected-character.ts
+++ src/hooks/use-selected-character.ts
@@ -1,6 +1,6 @@
 import { RootState } from '@/store';
 import { Character } from '@/types/d2l-types';
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 
 export default function useSelectedCharacter() {
@@ -9,7 +9,7 @@ export default function useSelectedCharacter() {
     (state: RootState) => state.dashboard.selectedCharacter
   );
 
-  const [selectedCharacter, setSelectedCharacter] = useState<Character | undefined>(characters[0]);
+  const [selectedCharacter, setSelectedCharacter] = useState<Character | undefined>(undefined);
 
   useEffect(() => {
     setSelectedCharacter(characters[selectedCharacterIndex]);
